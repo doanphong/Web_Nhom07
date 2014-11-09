7a8c4ed154e7b2a69fc3dc677ed5cb7a85b68346
doanphong <doanphongvn@gmail.com>
2014-11-09 23:01:55 +0700

    Revert "code"
    This reverts commit 389fd313f444b7166f8dab79685c5d03955a1896.

--- framework/admin/template/custom_fields/testimonial.php
+++
@@ -1,6 +0,0 @@
-<?php
-global $post;
-?>
-<label>Username Twitter:</label>
-<input type="text" name="username_twitter_testimonial" value="<?php echo get_post_meta($post->ID,THEME_SLUG.'username_twitter_testimonial',true);?>"/><br/>
-
--- framework/js/menu.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-    if(jQuery('.nav_root li').length > 0){
-        jQuery('.nav_root li:first-child').addClass('first');
-        jQuery('.nav_root li:last-child').addClass('last');
-    }
-});
-
